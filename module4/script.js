0f5e304a862abcba9aa85d6fa36ddb7129c8ba38
Md Salman Mansoori <salmansoori509@gmail.com>
2020-05-21 17:20:30 +0530

    This is my 4th Assignment

---
+++ module4/script.js
@@ -0,0 +1,17 @@
+(function (window) {
+
+
+var names = ["Salman", "John", "frk", "World", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i in names) {
+
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+ 
+  if (firstLetter == 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
