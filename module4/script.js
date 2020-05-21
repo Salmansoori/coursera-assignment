9ff312349eb4f85353ebd481cb1a8cbc6869cfaf
Md Salman Mansoori <salmansoori509@gmail.com>
2020-05-21 17:33:22 +0530

    Modified Files

--- module4/script.js
+++ module4/script.js
@@ -1,7 +1,7 @@
 (function (window) {
 
 
-var names = ["Salman", "John", "frk", "World", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i in names) {
 
